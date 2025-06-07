1cabba9ebc5b4f254de3f22d81b2710ed30667e1
holybaechu <holybaechu@gmail.com>
2025-06-07 22:25:54 +0900

    feat(web): Use different color for TitleBar's Button component

--- apps/web/src/lib/components/game/titleBar/Button.svelte
+++ apps/web/src/lib/components/game/titleBar/Button.svelte
@@ -5,9 +5,8 @@
 <button class={`
     px-1.5 h-auto
     text-xs 
-    bg-gray-250 hover:bg-gray-350
+    bg-gray-350 hover:bg-gray-300
     transition-all duration-200 ease-in-out
-    border-1 border-gray-400
     ${rightSide ? 'rounded-r-lg' : ''}
 `}>
     {@render children()}
