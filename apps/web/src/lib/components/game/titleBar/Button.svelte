b3d53d75c97d3b621fc7539d14a20ef5fa9ec8f5
holybaechu <holybaechu@gmail.com>
2025-06-07 22:04:04 +0900

    refactor(web): Migrate TitleBar components to use Tailwind CSS styles

--- apps/web/src/lib/components/game/titleBar/Button.svelte
+++ apps/web/src/lib/components/game/titleBar/Button.svelte
@@ -2,28 +2,13 @@
     let { children, rightSide = false } = $props();
 </script>
 
-<style>
-    .title-bar-button {
-        padding: 1px 15px;
-
-        font-size: inherit;
-        letter-spacing: inherit;
-
-        background: #d2d2d2;
-        border: 1px solid #bfbfbf;
-
-        transition: all 0.2s ease;
-    }
-
-    .title-bar-button.right-side {
-        border-radius: 0px 5px 5px 0px;
-    }
-
-    .title-bar-button:hover {
-        background: #EEEEEE;
-    }
-</style>
-
-<button class={`title-bar-button ${rightSide ? 'right-side' : ''}`}>
+<button class={`
+    px-1.5 h-auto
+    text-xs 
+    bg-gray-250 hover:bg-gray-350
+    transition-all duration-200 ease-in-out
+    border-1 border-gray-400
+    ${rightSide ? 'rounded-r-lg' : ''}
+`}>
     {@render children()}
 </button>
